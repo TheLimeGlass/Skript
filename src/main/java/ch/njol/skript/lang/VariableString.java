207018cd750ffe280d3b15036edb00dd8f6d34d3
TheLimeGlass <seantgrover@gmail.com>
2022-06-26 01:39:25 -0600

    Merge branch 'master' into DefaultVariablesFix

--- src/main/java/ch/njol/skript/lang/VariableString.java
+++ src/main/java/ch/njol/skript/lang/VariableString.java
@@ -31,6 +31,7 @@ import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.expressions.ExprColoured;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.ConvertedExpression;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.log.BlockingLogHandler;
 import ch.njol.skript.log.RetainingLogHandler;
