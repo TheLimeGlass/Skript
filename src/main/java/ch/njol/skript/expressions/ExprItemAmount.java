e53250c81dc3ae7799f120e183a6bae7dd140734
TheLimeGlass <seantgrover@gmail.com>
2022-07-12 01:05:43 -0600

    Fix up item amount expression

--- src/main/java/ch/njol/skript/expressions/ExprItemAmount.java
+++ src/main/java/ch/njol/skript/expressions/ExprItemAmount.java
@@ -18,91 +18,134 @@
  */
 package ch.njol.skript.expressions;
 
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+
+import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.ItemType;
-import ch.njol.skript.classes.Changer;
-import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.util.Getter;
 import ch.njol.skript.util.slot.Slot;
-import ch.njol.util.coll.CollectionUtils;
-import org.bukkit.event.Event;
-import org.eclipse.jdt.annotation.Nullable;
+import ch.njol.util.Kleenean;
 
 @Name("Item Amount")
 @Description("The amount of an <a href='classes.html#itemstack'>item stack</a>.")
-@Examples("send \"You have got %item amount of player's tool% %player's tool% in your hand!\" to player")
-@Since("2.2-dev24")
-public class ExprItemAmount extends SimplePropertyExpression<Object, Long> {
-	
-    static {
-        register(ExprItemAmount.class, Long.class, "item[[ ]stack] (amount|size|number)", "slots/itemtypes");
-    }
+@Examples({"send \"You have got %item amount of player's tool% %player's tool% in your hand!\" to player",
+		"send \"You have got %item amount of stone in player's inventory% stone in your inventory!\" to player"})
+@Since("2.2-dev24, INSERT VERSION (ItemType in Slots/Inventories)")
+public class ExprItemAmount extends SimpleExpression<Integer> {
 
-	
-	@Override
-	public Long convert(final Object item) {
-    	return (long) (item instanceof ItemType ? ((ItemType) item).getAmount() : ((Slot) item).getAmount());
+	static {
+		Skript.registerExpression(ExprItemAmount.class, Integer.class, ExpressionType.PROPERTY, "item[[ ]stack] (amount|size|number) [of %slots/itemtypes%]", "%slots/itemtypes%'[s] item[[ ]stack] (amount|size|number)");
+		Skript.registerExpression(ExprItemAmount.class, Integer.class, ExpressionType.COMBINED, "item[[ ]stack] (amount|size|number) of %itemtypes% [with]in %slots/inventories%");
 	}
-	
-	@Override
-    public @Nullable Class<?>[] acceptChange(Changer.ChangeMode mode) {
-        return (mode != ChangeMode.REMOVE_ALL) ? CollectionUtils.array(Number.class) : null;
-    }
 
-    @Override
-    public void change(Event event, @Nullable Object[] delta, Changer.ChangeMode mode) {
-    	int amount = delta != null ? ((Number) delta[0]).intValue() : 0;
-        switch (mode) {
-            case ADD:
-            	for (Object obj : getExpr().getArray(event))
-					if (obj instanceof ItemType) {
-						ItemType item = ((ItemType) obj);
-						item.setAmount(item.getAmount() + amount);
-					} else {
-						Slot slot = ((Slot) obj);
-						slot.setAmount(slot.getAmount() + amount);
-					}
-                break;
-            case SET:
-				for (Object obj : getExpr().getArray(event))
-					if (obj instanceof ItemType)
-						((ItemType) obj).setAmount(amount);
-					else
-						((Slot) obj).setAmount(amount);
-                break;
-            case REMOVE:
-				for (Object obj : getExpr().getArray(event))
-					if (obj instanceof ItemType) {
-						ItemType item = ((ItemType) obj);
-						item.setAmount(item.getAmount() - amount);
-					} else {
-						Slot slot = ((Slot) obj);
-						slot.setAmount(slot.getAmount() - amount);
-					}
-                break;
-            case REMOVE_ALL:
-            case RESET:
-			case DELETE:
-				for (Object obj : getExpr().getArray(event))
-					if (obj instanceof ItemType)
-						((ItemType) obj).setAmount(1);
-					else
-						((Slot) obj).setAmount(1);
-				break;
-        }
-    }
+	@Nullable
+	private Expression<ItemType> itemtype;
+	private Expression<Object> objects;
+	private int pattern;
 
+	private final Getter<Integer, Object> itemTypeGetter = new Getter<>() {
+		@Override
+		public @Nullable Integer get(Object object) {
+			if (object instanceof ItemType)
+				return ((ItemType) object).getAmount();
+			assert object instanceof Slot;
+			return ((Slot) object).getAmount();
+		}
+	};
+
+	@SuppressWarnings("unchecked")
 	@Override
-	public Class<? extends Long> getReturnType() {
-		return Long.class;
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		pattern = matchedPattern;
+		if (matchedPattern == 0) {
+			objects = (Expression<Object>) exprs[0];
+			return true;
+		}
+		itemtype = (Expression<ItemType>) exprs[0];
+		objects = (Expression<Object>) exprs[1];
+		return true;
 	}
 
 	@Override
-	protected String getPropertyName() {
-		return "item[[ ]stack] (amount|size|number)";
+	protected @Nullable Integer[] get(Event event) {
+		return null;
 	}
+
+	@Override
+	public boolean isSingle() {
+		return true;
+	}
+
+	@Override
+	public Class<? extends Integer> getReturnType() {
+		return Integer.class;
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		return null;
+	}
+
+//	@Override
+//	public Long convert(Object item) {
+//		return (long) (item instanceof ItemType ? ((ItemType) item).getAmount() : ((Slot) item).getAmount());
+//	}
+//
+//	@Override
+//	public @Nullable Class<?>[] acceptChange(ChangeMode mode) {
+//		return (mode != ChangeMode.REMOVE_ALL) ? CollectionUtils.array(Number.class) : null;
+//	}
+//
+//	@Override
+//	public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
+//		int amount = delta != null ? ((Number) delta[0]).intValue() : 0;
+//		switch (mode) {
+//			case ADD:
+//				for (Object obj : getExpr().getArray(event))
+//					if (obj instanceof ItemType) {
+//						ItemType item = ((ItemType) obj);
+//						item.setAmount(item.getAmount() + amount);
+//					} else {
+//						Slot slot = ((Slot) obj);
+//						slot.setAmount(slot.getAmount() + amount);
+//					}
+//				break;
+//			case SET:
+//				for (Object obj : getExpr().getArray(event))
+//					if (obj instanceof ItemType)
+//						((ItemType) obj).setAmount(amount);
+//					else
+//						((Slot) obj).setAmount(amount);
+//				break;
+//			case REMOVE:
+//				for (Object obj : getExpr().getArray(event))
+//					if (obj instanceof ItemType) {
+//						ItemType item = ((ItemType) obj);
+//						item.setAmount(item.getAmount() - amount);
+//					} else {
+//						Slot slot = ((Slot) obj);
+//						slot.setAmount(slot.getAmount() - amount);
+//					}
+//				break;
+//			case REMOVE_ALL:
+//			case RESET:
+//			case DELETE:
+//				for (Object obj : getExpr().getArray(event))
+//					if (obj instanceof ItemType)
+//						((ItemType) obj).setAmount(1);
+//					else
+//						((Slot) obj).setAmount(1);
+//				break;
+//		}
+//	}
 
 }
